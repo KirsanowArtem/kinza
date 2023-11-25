b7da3a3ab19e720f06200573589861b2383b69e1
Lenovo T440 <mm@cc.com>
2023-11-25 17:52:08 +0200

    -comit 25 11 2023

---
+++ src/main/java/Lessons/Lessons19/Lessons19.java
@@ -0,0 +1,12 @@
+package Lessons.Lessons19;
+
+import Metods.Do;
+
+public class Lessons19 {
+    public static void main(String[] args) {
+        int[] n = {123,3,57,78,6,9476,34,-7,5,8,11,21,13};
+//        System.out.println(Do.intMaxMass(n));
+//        System.out.println(Do.intMinMass(n));
+        System.out.println(Do.intSumProstMass(n));
+    }
+}
